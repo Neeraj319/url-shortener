b03a96102057db6b4d1ccd5d8b3b51cf0a8cf76f
Neeraj319 <itskop520@gmail.com>
2021-09-13 12:32:59 +0545

    added get url function

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from typing import Optional
 from sqlalchemy.future import engine
 import random
 import string
-from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 
 
 sqlite_file_url = "sqlite:///app.db"
@@ -24,19 +24,27 @@ def create_shorten_url():
     lower_cases = string.ascii_lowercase
     upper_cases = string.ascii_uppercase
     shorten_url = "".join(
-        lower_cases[random.randint(0, 26)] + upper_cases[random.randint(0, 26)]
+        lower_cases[random.randint(1, 26)] + upper_cases[random.randint(1, 26)]
         for _ in range(4)
     )
     return shorten_url
 
 
-create_shorten_url()
-
-
 def add_to_db(url: str):
 
-    url = Url(url=url, shorten_url=create_shorten_url(url=url))
+    url = Url(url=url, shorten_url=create_shorten_url())
 
     with Session(engine) as session:
         session.add(url)
         session.commit()
+
+
+def get_all_url():
+    with Session(engine) as session:
+        return [obj for obj in session.exec(select(Url))]
+
+
+def get_url(shoreten_link: str):
+    with Session(engine) as session:
+        url = session.exec(select(Url).where(Url.shorten_url == shoreten_link))
+        return url
